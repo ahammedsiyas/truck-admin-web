91df1234459cca1d83d575f663c46207fbab7315
ahammedsiyas <sirajfamina@gmail.com>
2026-02-02 00:28:18 +0530

    errorl commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+// http://localhost:5000/api
 const api = axios.create({
-  baseURL: 'http://3.80.95.96:5000',
+  baseURL: 'http://3.80.95.96:5000/api',
 });
 
 api.interceptors.request.use((config) => {
